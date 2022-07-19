23ac1231d6c01c261cb8a065e0473a960527258a
sivanandha <chillapothusiva411@gmail.com>
2022-07-19 12:13:06 +0530

    this is my  first commit

---
+++ src/sample_pattern/simple.java
@@ -0,0 +1,14 @@
+package sample_pattern;
+
+public class simple {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j <= n; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
